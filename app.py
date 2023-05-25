3fb888d73ddcffc9f55b2c84f1d39008c26670c8
Omowaye Victor <114419607+Wayevee@users.noreply.github.com>
2023-05-25 14:37:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def local_css(file_name):
         with open(file_name) as f:
             slt.markdown(f"<style>{f.read()}</style>",unsafe_allow_html=True)
 
-local_css("0m0waye\style.css")
+local_css(f'0m0waye/style.css')
 
 with slt.container():
     slt.subheader ("HI I am Omowaye Victor :wave:")
